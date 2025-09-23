b7ed16d3948ff93541c6266793e78687eec9e4b2
Ricson_dabre <132834752+Ricsondabre@users.noreply.github.com>
2025-09-23 08:59:37 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,17 +1,17 @@
 import pygame
 import sys
-from player import Player
+from Player import Player
 
 pygame.init()
-screenX, screenY = 900, 900
+screenX, screenY = 900, 800
 gameScreen = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption("Platformer")
 
 tickSpeed = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
-playerOne = Player(pos=(100, 700), size=64, playernum=1)
-playerTwo = Player(pos=(300, 700), size=64, playernum=2)
+playerOne = Player(pos=(100, 800), size=64, playernum=1)
+playerTwo = Player(pos=(300, 800), size=64, playernum=2)
 all_sprites.add(playerOne)
 all_sprites.add(playerTwo)
 running = True
