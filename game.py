542b8b7b43fd01ad589fb24a97339bfbb02bd9e5
zachdolandevacc <zachdolan.online.devacc@gmail.com>
2025-09-23 08:29:48 -0500

    Fix game.py bugs
    - changed capitalization of line 3 from "Player" to "player"

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
-from Player import Player
+from player import Player
 
 pygame.init()
-screenX, screenY = 900, 800
+screenX, screenY = 900, 900
 gameScreen = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption("Platformer")
 
@@ -13,6 +13,7 @@ all_sprites = pygame.sprite.Group()
 player = Player(pos=(100, 700), size=64)
 all_sprites.add(player)
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,5 +23,6 @@ while running:
     all_sprites.draw(gameScreen)
     pygame.display.flip()
     tickSpeed.tick(60)
+    
 pygame.quit()
 sys.exit()
