bb9e4145cade161b31c099ce40cea2052b8857ae
Ricson_dabre <132834752+Ricsondabre@users.noreply.github.com>
2025-09-23 08:36:22 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,28 +1,26 @@
 import pygame
 import sys
-from player import Player
+from Player import Player
 
 pygame.init()
-screenX, screenY = 900, 900
+screenX, screenY = 800, 800
 gameScreen = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption("Platformer")
 
 tickSpeed = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
-player = Player(pos=(100, 700), size=64)
+player = Player(pos=(100, 800), size=64)
 all_sprites.add(player)
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     all_sprites.update()
-    gameScreen.fill((0, 0, 0))
+    gameScreen.fill((244, 194, 194))
     all_sprites.draw(gameScreen)
     pygame.display.flip()
     tickSpeed.tick(60)
-    
 pygame.quit()
 sys.exit()
