caf8937b8080c781f5f49aac4d0596656812f705
zachdolandevacc <zachdolan.online.devacc@gmail.com>
2025-09-23 08:34:44 -0500

    Condense player.py
    - removed "dead" variables, optimized code in the true loop by removing unnecessary conditions

--- player.py
+++ player.py
@@ -15,24 +15,19 @@ class Player(pygame.sprite.Sprite):
         self.jumpCooldown = 0
         self.health = 3
         self.respawn_position = pos
-        self.score = 0
-        self.is_dead = False
-        self.invincible = False
-        self.invincibility_timer = 0
 
     def getInput(self):
-        if not self.is_dead:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP]:
-                self._jump()
-            if keys[pygame.K_LEFT]:
-                self.direction.x = -1
-                self.facingRight = False
-            elif keys[pygame.K_RIGHT]:
-                self.direction.x = 1
-                self.facingRight = True
-            else:
-                self.direction.x = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self._jump()
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.facingRight = False
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.facingRight = True
+        else:
+            self.direction.x = 0
 
     def _jump(self):
         if self.onGround and self.jumpCooldown == 0:
@@ -40,41 +35,18 @@ class Player(pygame.sprite.Sprite):
             self.onGround = False
             self.jumpCooldown = 45
 
-    def loseHealth(self):
-        if not self.is_dead:
-            self.health -= 1
-            if self.health <= 0:
-                self.die()
-            else:
-                self.respawn()
-
-    def die(self):
-        self.is_dead = True
-        print("Game Over!")
-
     def respawn(self):
-        self.is_dead = False
         self.rect.topleft = self.respawn_position
-        self.invincible = True
-        self.invincibility_timer = 60
 
     def update(self):
-        if not self.is_dead:
-            self.getInput()
-            if self.jumpCooldown > 0:
-                self.jumpCooldown -= 1
-            self.rect.x += self.direction.x * self.speed
-            self.rect.y += self.direction.y
-            if not self.onGround:
-                self.direction.y += 1
-            if self.rect.bottom >= 800:
-                self.rect.bottom = 800
-                self.direction.y = 0
-                self.onGround = True
-            if self.invincible:
-                self.invincibility_timer -= 1
-                if self.invincibility_timer <= 0:
-                    self.invincible = False
-
-    def increaseScore(self, amount):
-        self.score += amount
+        self.getInput()
+        if self.jumpCooldown > 0:
+            self.jumpCooldown -= 1
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y
+        if not self.onGround:
+            self.direction.y += 1
+        if self.rect.bottom >= 800:
+            self.rect.bottom = 800
+            self.direction.y = 0
+            self.onGround = True
