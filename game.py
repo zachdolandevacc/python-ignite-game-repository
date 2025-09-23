9e2f44177ea74b0d9f61cb92cb6405cb3fabec23
zachdolandevacc <zachdolan.online.devacc@gmail.com>
2025-09-23 08:51:55 -0500

    Update game.py
    - adds 4 lines of code, shouldn't break anything. hopefully.

--- game.py
+++ game.py
@@ -1,26 +1,30 @@
 import pygame
 import sys
-from Player import Player
+from player import Player
 
 pygame.init()
-screenX, screenY = 800, 800
+screenX, screenY = 900, 900
 gameScreen = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption("Platformer")
 
 tickSpeed = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
-player = Player(pos=(100, 800), size=64)
-all_sprites.add(player)
+playerOne = Player(pos=(100, 700), size=64, playernum=1)
+playerTwo = Player(pos=(300, 700), size=64, playernum=2)
+all_sprites.add(playerOne)
+all_sprites.add(playerTwo)
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     all_sprites.update()
-    gameScreen.fill((244, 194, 194))
+    gameScreen.fill((0, 0, 0))
     all_sprites.draw(gameScreen)
     pygame.display.flip()
     tickSpeed.tick(60)
+    
 pygame.quit()
 sys.exit()
